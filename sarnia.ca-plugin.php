91446c3b8b4adf2ae83d5eddd75c4135dd5d2939
Zachary Collette <zachary.collette@sarnia.ca>
2019-04-09 16:18:45 +0000

    Initial commit

---
+++ sarnia.ca-plugin.php
@@ -0,0 +1,21 @@
+<?php
+/*Plugin Name: Sarnia.ca Plugin
+* Description: This plugin contains the functionality code required for the sarnia.ca website
+* Version: 0.1
+* Author: Zachary Collette
+* Author URI: http://sarnia.ca
+*/
+add_action( 'wp_head', 'sarnia_add_analytics' );
+function sarnia_add_analytics() {
+  ?>
+  <!-- Global Site Tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=<?=getenv('google.analytics.trackingID')?>"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '<?=getenv('google.analytics.trackingID')?>');
+  </script>
+  <?php
+}
