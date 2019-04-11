a9d6a19f812d8b922286752fbb60a3ee6ef761f7
Zachary Collette <zachary.collette@sarnia.ca>
2019-04-11 19:22:58 +0000

    disable search

--- sarnia.ca-plugin.php
+++ sarnia.ca-plugin.php
@@ -8,8 +8,6 @@
 
 define( 'SARNIA_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
-add_action( 'wp_head', 'sarnia_add_analytics' );
-
 function sarnia_add_analytics() {
   ?>
   <!-- Global Site Tag (gtag.js) - Google Analytics -->
@@ -23,3 +21,20 @@ function sarnia_add_analytics() {
   </script>
   <?php
 }
+add_action( 'wp_head', 'sarnia_add_analytics' );
+
+function fb_filter_query( $query, $error = true ) {
+
+	if ( is_search() ) {
+		$query->is_search = false;
+		$query->query_vars = false;
+		$query->query = false;
+
+		// to error
+		if ( $error == true )
+			$query->is_404 = true;
+	}
+}
+
+add_action( 'parse_query', 'fb_filter_query' );
+add_filter( 'get_search_form', function() { return null;} );
