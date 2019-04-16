21697181ad1637af00f9bbba2d4223c43f431647
Zachary Collette <zachary.collette@sarnia.ca>
2019-04-16 17:30:40 +0000

    remove 404 redirect on search query, no longer needed.  redirect functionality now in the safe redirect manager plugin

--- sarnia.ca-plugin.php
+++ sarnia.ca-plugin.php
@@ -22,19 +22,3 @@ function sarnia_add_analytics() {
   <?php
 }
 add_action( 'wp_head', 'sarnia_add_analytics' );
-
-function fb_filter_query( $query, $error = true ) {
-
-	if ( is_search() ) {
-		$query->is_search = false;
-		$query->query_vars = false;
-		$query->query = false;
-
-		// to error
-		if ( $error == true )
-			$query->is_404 = true;
-	}
-}
-
-add_action( 'parse_query', 'fb_filter_query' );
-add_filter( 'get_search_form', function() { return null;} );
