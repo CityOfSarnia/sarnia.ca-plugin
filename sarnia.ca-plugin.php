47e1493e37cb097e8aaa424ea0068d3e4c5b8d84
Zachary Collette <zachary.collette@sarnia.ca>
2019-05-29 10:06:28 -0400

    add gform snippet for X_FORWARDED_FOR header

--- sarnia.ca-plugin.php
+++ sarnia.ca-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*Plugin Name: Sarnia.ca Plugin
 * Description: This plugin contains the functionality code required for the sarnia.ca website
-* Version: 0.2
+* Version: 0.1.3
 * Author: City of Sarnia
 * Author URI: http://sarnia.ca
 */
@@ -27,8 +27,7 @@ add_filter( 'gform_field_value_refurl', 'populate_referral_url');
 
 function populate_referral_url( $form ){
     // Grab URL from HTTP Server Var and put it into a variable
-    $refurl = htmlspecialchars($_SERVER['HTTP_REFERER']);
-//    $refurl = $_SERVER['HTTP_REFERER'];
+    $refurl = $_SERVER['HTTP_REFERER'];
 
     // Return that value to the form
     return esc_url_raw($refurl);
@@ -37,10 +36,16 @@ function populate_referral_url( $form ){
 add_filter( 'gform_field_value_reftitle', 'populate_page_title');
 
 function populate_page_title( $form ){
-    $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
     $back_id = url_to_postid($_SERVER['HTTP_REFERER']);
     if( $back_id > 0 ) $back_title = get_the_title( $back_id );
     // Return that value to the form
     return $back_title;
 }
 
+add_filter( 'gform_ip_address', 'filter_gform_ip_address' );
+
+function filter_gform_ip_address( $ip ) {
+    // Return the IP address set by the proxy.
+    // E.g. $_SERVER['HTTP_X_FORWARDED_FOR'] or $_SERVER['HTTP_CLIENT_IP']
+    return $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
