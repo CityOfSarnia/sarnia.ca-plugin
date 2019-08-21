f5f3de82a256c0f19a1f80195ec025af2077346f
Zachary Collette <zachary.collette@sarnia.ca>
2019-08-21 17:08:24 -0400

    Add functions, assets and templates Moved core website functionality out of the theme and into the plugin.  This gives more flexibility in the future, and content will still be visible if the theme is changed

--- sarnia.ca-plugin.php
+++ sarnia.ca-plugin.php
@@ -17,6 +17,222 @@ add_filter('bedrock/stage_switcher_visibility', function ($visibility) {
     return true;
 });
 
+use function \Sober\Intervention\intervention;
+
+if (function_exists('\Sober\Intervention\intervention')) {
+    // now you can use the function to call the required modules and their params
+    intervention('remove-menu-items', 'plugins', ['editor', 'author']);
+    //intervention('remove-menu-items', 'plugins', 'all');
+    intervention('remove-emoji');
+    // Removes howdy and replaces with Hello.
+    intervention('remove-howdy', 'Hello');
+}
+
+function card_colour()
+{
+	global $card_count;
+
+	$card_count++;
+
+	switch ($card_count) {
+		case 1:
+			$colour = 'card--blue';
+			break;
+		case 2:
+			$colour = 'card--red';
+			break;
+		case 3:
+			$colour = 'card--yellow';
+			$card_count = 0;
+			break;
+		default:
+			$card_count = 0;
+	}
+
+	return $colour;
+}
+
+add_image_size('home-banner', 1000, 900, true);
+add_image_size('banner', 1600, 600, true);
+add_image_size('card', 780, 200, true);
+
+// Add support for page excerpts
+add_post_type_support('page', 'excerpt');
+
+function create_my_post_types()
+{
+    register_post_type(
+        'notifications',
+        array(
+            'labels' => array(
+                'name' => __('Notifications'),
+                'singular_name' => __('Notification')
+            ),
+            'public' => true,
+            'menu_icon' => 'dashicons-star-filled',
+            'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+            'exclude_from_search' => true,
+            'show_in_admin_bar'   => false,
+            'show_in_nav_menus'   => false,
+            'publicly_queryable'  => true,
+            'query_var'           => false,
+        )
+    );
+}
+add_action('init', 'create_my_post_types');
+
+function build_taxonomies()
+{
+    register_taxonomy(
+        'filter',
+        array('notifications'),
+        array(
+            'hierarchical' => true,
+            'label' => 'Filter',
+            'query_var' => true,
+        )
+    );
+    register_taxonomy(
+        'notification-icon',
+        array('notifications'),
+        array(
+            'hierarchical' => true,
+            'label' => 'Notification Icon',
+            'query_var' => true,
+        )
+    );
+}
+// Create Custom Taxonomies
+add_action('init', 'build_taxonomies', 0);
+
+// Register Options Page
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page();
+}
+
+function my_acf_json_load_point($paths)
+{
+    // remove original path (optional)
+    unset($paths[0]);
+    $paths[] = plugin_dir_path(__FILE__) . '/assets/acf-json';
+
+    return   $paths;
+}
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_save_point($path)
+{
+    // update path
+    $path = plugin_dir_path(__FILE__) . '/assets/acf-json';
+
+    // return
+    return $path;
+}
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+function my_acf_block_render_callback($block)
+{
+
+    // convert name ("acf/testimonial") into path friendly slug ("testimonial")
+    $slug = str_replace('acf/', '', $block['name']);
+
+    // include a template part from within the "template-parts/block" folder
+    if (file_exists(plugin_dir_path(__FILE__) . "/block/block-{$slug}.php")) {
+        include(plugin_dir_path(__FILE__) . "/block/block-{$slug}.php");
+    }
+}
+
+function my_acf_init()
+{
+
+    // check function exists
+    if (function_exists('acf_register_block')) {
+
+        // register a post card block
+        acf_register_block(array(
+            'name'                        => 'post-card',
+            'title'                        => __('Post Card'),
+            'description'            => __('A post or page card block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'media-default',
+            'keywords'                => array('post', 'card'),
+            'supports'                 => array('align' => false),
+        ));
+
+        // register a custom card block
+        acf_register_block(array(
+            'name'                        => 'custom-card',
+            'title'                        => __('Custom Card'),
+            'description'            => __('A custom card block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'media-text',
+            'keywords'                => array('custom', 'card'),
+            'supports'                 => array('align' => false),
+        ));
+
+        // register an accordion block
+        acf_register_block(array(
+            'name'                        => 'accordion',
+            'title'                        => __('Accordion'),
+            'description'            => __('An accordion block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'plus',
+            'keywords'                => array('accordion', 'toggle', 'dropdown'),
+            'supports'                 => array('align' => false),
+        ));
+
+        // register a notifications block
+        acf_register_block(array(
+            'name'                        => 'notifications',
+            'title'                        => __('Notifications'),
+            'description'            => __('A notifications block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'star-filled',
+            'keywords'                => array('notifications'),
+            'supports'                 => array('align' => array('full')),
+        ));
+
+        // register a recent posts block
+        acf_register_block(array(
+            'name'                        => 'recent-posts',
+            'title'                        => __('Recent Posts'),
+            'description'            => __('Recent posts by category block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'admin-page',
+            'keywords'                => array('recent', 'posts', 'news'),
+            'supports'                 => array('align' => array('wide')),
+        ));
+
+        // register a navigation block
+        acf_register_block(array(
+            'name'                        => 'navigation',
+            'title'                        => __('Navigation'),
+            'description'            => __('A navigation block.'),
+            'render_callback'    => 'my_acf_block_render_callback',
+            'category'                => 'formatting',
+            'icon'                        => 'list-view',
+            'keywords'                => array('navigation', 'menu', 'nav'),
+            'supports'                 => array('align' => false),
+        ));
+    }
+}
+add_action('acf/init', 'my_acf_init');
+
+function my_acf_post_id()
+{
+	if (is_admin() && function_exists('acf_maybe_get_POST')) :
+		return intval(acf_maybe_get_POST('post_id'));
+	else :
+		global   $post;
+		return   $post->ID;
+	endif;
+}
+
 function get_notifications($request)
 {
     $data = get_posts(array(
@@ -126,9 +342,10 @@ function wrap_gform_cdata_close($content = '')
 
 // Return the IP address set by the proxy.
 add_filter('gform_ip_address', 'filter_gform_ip_address');
-function filter_gform_ip_address($ip) {
-    if( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) )
+function filter_gform_ip_address($ip)
+{
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
         return $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else  
+    else
         return FALSE;
 }
