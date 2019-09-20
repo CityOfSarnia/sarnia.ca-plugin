1301699f72b6474b3bd54a581aa03d0df524dc33
douglas <andrew.douglas@sarnia.ca>
2019-09-20 13:02:47 -0400

    Inital creation of shortcut-menu ACF, and accompanying files Includes shortcut-menu.scss, block-shortcut-menu.php for implementation as well as initalization in sarnia-ca-plugin.php

---
+++ includes/block/block-shortcut-menu.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Block Name: Shortcut Menu
+ *
+ * This is the template that displays the shortcut-menu block.
+ */
+?>
+<div class="shortcut-menu alignwide">
+	<ul>
+		<?php if( have_rows('menu_repeater') ):?>
+			
+			<?php while( have_rows('menu_repeater') ): the_row(); 
+
+				// vars
+				$image = get_sub_field('sub_field_image');
+				$text = get_sub_field('sub_field_text');
+				$link = get_sub_field('sub_field_url');
+
+				?>
+
+					<?php if( $link ): ?>
+						<a href="<?php echo $link; ?>">
+					<?php endif; ?>
+						<li class = "shortcut-menu-list__item">
+							
+							<img id="<?= $image['url'] ?>" src="<?php echo $image['url']; ?>" width="100px" height="100px" />
+							<span class="shortcut-menu-li caption"> <?= $text; ?> </span>
+							
+						</li>
+					<?php if( $link ): ?>
+						</a>
+					<?php endif; ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</ul>
+ </div><!-- .shortcut-menu -->
